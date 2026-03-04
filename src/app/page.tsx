0439989e4dea48d787553497004234fa518ac5ec
Adeyemi Ogunnaike <adeyemiogunnaike02@gmail.com>
2026-03-04 10:54:22 +0100

    little change

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex items-center justify-center">
+<p className="text-red-800 text-[8rem] m-0">YOU CAN AND YOU WILL</p>
+
+   </div>
+  );
+}
